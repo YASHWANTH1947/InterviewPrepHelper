cea231e0a04d1b57b9b648d19c2e2400c1507942
Yashwanth <yashwanthreddy1947@gmail.com>
2026-04-14 14:29:30 +0530

    Some deployment bugs fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import aiRoutes from "./routes/interviewReport.routes.js";
 import connectDB from "./db/index.js";
-import cookeParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -13,17 +13,18 @@ const app = express();
 
 app.use(
   cors({
-    origin:
-      process.env.FRONTEND_URL ||
-      "http://localhost:5173" ||
+    origin: [
+      process.env.FRONTEND_URL,
+      "http://localhost:5173",
       "https://interview-prep-helper.vercel.app",
+    ].filter(Boolean),
     credentials: true, // Essential for sending/receiving cookies
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 app.use(express.json());
-app.use(cookeParser());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/interviewReport", aiRoutes);
 app.get("/", (req, res) => {
